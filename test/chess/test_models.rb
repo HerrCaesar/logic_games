fe13cd9dee7f59bc8463cdbef12a43cd7b6df661
Zac <zac4793@gmail.com>
2019-08-23 09:37:03 +0100

    Start building tests for chess

---
+++ test/chess/test_models.rb
@@ -0,0 +1,35 @@
+require 'minitest/autorun'
+# Game
+class GameTest < Minitest::Test
+
+end
+
+# Board
+class BoardTest < Minitest::Test
+
+end
+
+# Graveyard
+class GraveyardTest < Minitest::Test
+
+end
+
+# Record
+class RecordTest < Minitest::Test
+
+end
+
+# MoveMaker
+class MoveMakerTest < Minitest::Test
+
+end
+
+# Move
+class MoveTest < Minitest::Test
+
+end
+
+# MoveAlgebra
+class MoveAlgebraTest < Minitest::Test
+
+end
